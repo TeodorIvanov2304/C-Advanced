c178895b444380791604df6bb94490ca3f50cfec
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-03 15:41:15 +0200

    Creating Advanced Folder and 01. Problem

---
+++ Advanced/1. Stacks and Queues/1. Stacks and Queues - Lab/01. Reverse a String.cs
@@ -0,0 +1,27 @@
+﻿namespace _1._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Четем входа 
+            string input = Console.ReadLine();
+            //Създаваме stack от char, в който ще пазим символите
+            Stack<char> charStack = new Stack<char>();
+
+            //Създаваме foreach, който върти по дължината на input-а
+            foreach (char ch in input)
+            {   
+                // Push-ваме всеки един от елеметните на входа, и понеже стака слага всичко най-отгоре
+                // се получава обърнат стринг
+                charStack.Push(ch);
+            }
+
+            // Изпечатваме обърнатия input с while-loop
+            while (charStack.Count > 0)
+            {
+                Console.Write(charStack.Pop());
+            }
+        }
+    }
+}
