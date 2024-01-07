381c6b5e7d68d95161528b791df1e23e07abb33f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-07 18:28:37 +0200

    Create 05. Fashion Boutique.cs

---
+++ Advanced/1. Stacks and Queues/Stacks and Queues - Exercise/05. Fashion Boutique.cs
@@ -0,0 +1,39 @@
+﻿namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] boxWithClothes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int capacity = int.Parse(Console.ReadLine());
+            int currentRack = 0;
+            int racks = 1;
+            Stack<int> stack = new Stack<int>(boxWithClothes);
+
+            while (stack.Count>0)
+            {
+                int currentCloth = stack.Peek();
+
+                if (currentRack + currentCloth <= capacity) 
+                {
+                    stack.Pop();
+                    currentRack += currentCloth;
+                }
+                else
+                {
+                    racks++;
+                    currentRack = 0;
+                    stack.Pop();
+                    currentRack += currentCloth;
+                    
+                }
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
