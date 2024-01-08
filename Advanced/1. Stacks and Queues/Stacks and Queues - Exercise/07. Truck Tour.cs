6a6aa71eac3f6ac244d482b6216f7b0b39a40d94
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-08 13:12:32 +0200

    Adding two Problems

---
+++ Advanced/1. Stacks and Queues/Stacks and Queues - Exercise/07. Truck Tour.cs
@@ -0,0 +1,63 @@
+﻿namespace _07._Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int petrolPumpsCount = int.Parse(Console.ReadLine());
+            Queue<int[]> pumpsAndFuel = new Queue<int[]>();
+            //Queue<int, int> pumpsAndFuel2 = new Queue<int,int>(); Може да се направи и с този вариант
+
+            for (int i = 0; i < petrolPumpsCount; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int petrol = input[0];
+                int distance = input[1];
+
+                pumpsAndFuel.Enqueue(input);
+
+            }
+
+            int bestRoute = 0;
+
+            while (true)
+            {
+                int totalPetrol = 0;
+
+                foreach (int[] pump in pumpsAndFuel)
+                {
+                    totalPetrol += pump[0];
+                    int currentDistance = pump[1];
+
+                    //Ако горивото няма да стигне, брейкваме и отиваме на следващия foreach
+                    if (totalPetrol - currentDistance < 0)
+                    {
+                        totalPetrol = 0;
+                        break;
+                    }
+                    //Ако е достатъчно, вадима разстоянието от горивото
+                    else
+                    {
+                        totalPetrol -= currentDistance;
+                    }
+
+                    
+                }
+                //Ако горивото е достатъчно, излизаме от while
+                if (totalPetrol > 0)
+                {
+                    break;
+                }
+                bestRoute++;
+                //Добавяме в опашката най-отзад премахнатия pumpsAndFuel.Dequeue() най-отпред
+                pumpsAndFuel.Enqueue(pumpsAndFuel.Dequeue());
+            }
+        
+            Console.WriteLine(bestRoute);
+        }
+    }
+}
---
+++ Advanced/1. Stacks and Queues/Stacks and Queues - Exercise/08. Balanced Parenthesis.cs
@@ -0,0 +1,51 @@
+﻿namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> chars = new Stack<char>();
+            foreach (char item in input)
+            {   
+
+                //Добавяме отварящите скоби в стека
+                if (item == '('
+                    || item == '['
+                    ||item =='{')
+                {
+                    chars.Push(item);
+                    continue;
+                }
+                //Ако скобите са огледални, но има още затварящи скоби, ще излезне че са огледални. Но те не са, защото има една затваряща скоба повече, която няма да бъде добавена.За това просто добавяме скобата с push, и минаваме в крайния Else
+                if (chars.Count == 0)
+                {   
+                    chars.Push(item);
+                    break;
+                }
+                if (item == ')' && chars.Peek() == '(')
+                {
+                    chars.Pop();
+                }
+                else if (item == ']' && chars.Peek() == '[')
+                {
+                    chars.Pop();
+                }
+                else if (item == '}' && chars.Peek() == '{')
+                {
+                    chars.Pop();
+                }
+                
+            }
+
+            if (chars.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
