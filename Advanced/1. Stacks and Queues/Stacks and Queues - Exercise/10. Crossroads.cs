ee2faaed2702a5aa142157f86b09802ec98ae7f4
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-08 15:24:18 +0200

    Create 10. Crossroads.cs

---
+++ Advanced/1. Stacks and Queues/Stacks and Queues - Exercise/10. Crossroads.cs
@@ -0,0 +1,56 @@
+﻿namespace _10._Crossroads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenDuration = int.Parse(Console.ReadLine());
+            int freeWindowDuration = int.Parse(Console.ReadLine());
+            int totalCarsPassed = 0;
+            Queue<string> cars = new Queue<string>();
+            string input = Console.ReadLine();
+
+            while (input  != "END")
+            {
+                if (input != "green")
+                {
+                    cars.Enqueue(input);
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                int currentGreenLightDuration = greenDuration;
+
+                while(cars.Count > 0 && currentGreenLightDuration > 0)
+                {
+                    string currentCar = cars.Dequeue();
+
+                    if (currentGreenLightDuration - currentCar.Length > 0)
+                    {
+                        currentGreenLightDuration -= currentCar.Length;
+                        totalCarsPassed++;
+                        continue;
+                    }
+
+                    else if(currentGreenLightDuration + freeWindowDuration - currentCar.Length >= 0)
+                    {
+                        totalCarsPassed++;
+                        break;
+                    }
+
+                    else
+                    {
+                        char hittedSymbol = currentCar[currentGreenLightDuration + freeWindowDuration];
+                        Console.WriteLine("A crash happened!");
+                        Console.WriteLine($"{currentCar} was hit at {hittedSymbol}.");
+                        return;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Everyone is safe.");
+            Console.WriteLine($"{totalCarsPassed} total cars passed the crossroads.");
+        }
+    }
+}
