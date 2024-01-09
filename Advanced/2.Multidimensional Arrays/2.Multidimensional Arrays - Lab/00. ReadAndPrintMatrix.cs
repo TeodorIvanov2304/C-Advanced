7b37fee149dd93304f6df4b9694fc4c407896be9
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-09 19:58:00 +0200

    Creating Multidimensional Arrays folder and First Program

---
+++ Advanced/2.Multidimensional Arrays/2.Multidimensional Arrays - Lab/00. ReadAndPrintMatrix.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+
+namespace _00._ReadAndPrintMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrica = ReadMatrix(3,3);
+
+            PrintMatrix(matrica);
+        }
+
+        static int[,] ReadMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            return matrix;
+        }
+
+        static int[,] PrintMatrix(int[,] matrix) 
+        {   
+            //GetLength(0) взима дължината на редовете
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            return matrix;
+        }
+    }
+
+    
+}
