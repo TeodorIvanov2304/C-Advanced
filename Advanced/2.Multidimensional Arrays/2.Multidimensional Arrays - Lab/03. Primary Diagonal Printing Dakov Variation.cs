0952f1c734f134bb726e245588ac558832ed785f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-10 00:42:27 +0200

    Create 03. Primary Diagonal Printing Dakov Variation.cs

---
+++ Advanced/2.Multidimensional Arrays/2.Multidimensional Arrays - Lab/03. Primary Diagonal Printing Dakov Variation.cs
@@ -0,0 +1,29 @@
+﻿namespace _03._Primary_Diagonal_Dakov_Variation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsAndCols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rowsAndCols, rowsAndCols];
+
+            int sum = 0;
+
+            //Изпечатване на диагонал надясно
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write(matrix[i,i] + " ");
+            }
+
+            Console.WriteLine();
+
+            //Изпечатване на диагонал наляво
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write(matrix[i,matrix.GetLength(1)-i-1] + " ");
+            }
+            
+        }
+    }
+}
