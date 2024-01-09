59f809650edb7172094b4d65514e989a55a87c90
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-09 21:28:52 +0200

    Create 02. Sum Matrix Columns.cs

---
+++ Advanced/2.Multidimensional Arrays/2.Multidimensional Arrays - Lab/02. Sum Matrix Columns.cs
@@ -0,0 +1,42 @@
+﻿namespace _02._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Прочитаме матрицата и я запълваме по същия начин, като миналата задача
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = sizes[0];
+
+            int cols = sizes[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowValues = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+
+            //За да съберем колоните, обръщаме циклите външния цикъл върви по колоните, а вътрешния по редовете
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+
+                for (int row = 0; row < rows; row++)
+                {   
+                    //Събираме стойностите на колоните
+                    sum += matrix[row, col];
+                }
+                //Изпечатваме заедно с нов ред
+                Console.WriteLine();
+                Console.Write(sum);
+            }
+        }
+    }
+}
