1c6180169bb9b8153dcb57e55b088d5206fc0a6a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-11 14:22:24 +0200

    Create 02. Squares in Matrix.cs

---
+++ Advanced/2.Multidimensional Arrays/2.Multidimensional Arrays - Exercise/02. Squares in Matrix.cs
@@ -0,0 +1,45 @@
+﻿namespace _02._Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = rowsAndCols[0];
+            int cols = rowsAndCols[1];
+
+            string[,] matrix = new string [rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] rowValues = Console.ReadLine().Split();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+
+            int symbolSquaresCount = 0;
+
+            //Не включваме елемените, които са на последе ред и колониа, като пишем rows-1 и cols-1 в цикъла
+            for (int row = 0; row < rows-1; row++)
+            {
+                for (int col = 0; col < cols-1; col++)
+                {
+                    
+                    bool is2X2 = matrix[row, col] == matrix[row, col + 1]
+                        && matrix[row, col] == matrix[row + 1, col]
+                        && matrix[row, col] == matrix[row + 1, col + 1];
+ 
+                    if (is2X2)
+                    {
+                        symbolSquaresCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(symbolSquaresCount);
+        }
+    }
+}
