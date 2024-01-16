032f19033ca0bd67b9fe378b1b7f6465878e779f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-16 22:41:38 +0200

    Create 03. Largest 3 Numbers Short Variation.cs

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers Short Variation.cs
@@ -0,0 +1,19 @@
+﻿namespace _03._Largest_3_Numbers_Short_Variation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Чейнваме .Take(3), като 3 е бр. на желаните цифри, т.е трите най-големи
+            List<int> largestThreeNumbers = Console.ReadLine()
+                                            .Split()
+                                            .Select(int.Parse)
+                                            .OrderByDescending(x => x)
+                                            .Take(3) 
+                                            .ToList();
+
+            Console.WriteLine(string.Join(" ",largestThreeNumbers));
+
+        }
+    }
+}
