729f1708463f7fc2c831392bcc13629009a77f41
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-16 20:31:51 +0200

    Created Dictionary Advanced Folder and first Problem

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array.cs
@@ -0,0 +1,30 @@
+﻿namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                                          .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                                          .Select(double.Parse)
+                                          .ToList();
+
+            Dictionary<double,int> times = new Dictionary<double,int>();
+
+            foreach (var number in numbers) 
+            {
+                if (!times.ContainsKey(number))
+                {
+                    times[number] = 0;
+                }
+
+                times[number]++;
+            }
+
+            foreach (KeyValuePair<double,int> kvp in times)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
