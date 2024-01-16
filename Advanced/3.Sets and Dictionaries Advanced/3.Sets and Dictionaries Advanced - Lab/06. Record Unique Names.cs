e32263c01a82b0a91ee993ad7d1ccd7bb58eab70
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-17 01:46:58 +0200

    Create 06. Record Unique Names.cs

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Lab/06. Record Unique Names.cs
@@ -0,0 +1,24 @@
+﻿namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Четем броя на имената от конзолата
+            int numberOfNames = int.Parse(Console.ReadLine());
+            //Създаваме колекция от тип HashSet<string>
+            HashSet<string> names = new HashSet<string>();
+
+            
+            for (int i = 0; i < numberOfNames; i++)
+            {   
+                //Добавяме от конзолата направо в хешсета
+                names.Add(Console.ReadLine());
+            }
+
+            //Изпечатваме с нов ред и string.Join
+            //Console.WriteLine(string.Join("\n", names));
+
+        }
+    }
+}
