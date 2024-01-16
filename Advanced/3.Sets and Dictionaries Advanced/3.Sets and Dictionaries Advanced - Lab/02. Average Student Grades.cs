bade1167bbc89716aa37c909559dfac47aed82b8
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-16 22:14:18 +0200

    Create 02. Average Student Grades.cs

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Lab/02. Average Student Grades.cs
@@ -0,0 +1,37 @@
+﻿namespace _02._Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int students = int.Parse(Console.ReadLine());
+            //Незнайно по каква причина, когато беше double, не работи!
+            Dictionary<string,List<decimal>> grades = new Dictionary<string, List<decimal>>();
+            for (int i = 0; i < students; i++)
+            {   
+                string[] nameAndGrade = Console.ReadLine()
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = nameAndGrade[0];
+                //Незнайно по каква причина, когато беше double, не работи!
+                decimal grade = decimal.Parse(nameAndGrade[1]);
+
+                if (!grades.ContainsKey(name))
+                {   
+                    //Добавяме нов List<double> в който да държим оценките
+                    grades.Add(name, new List<decimal>());
+                    grades[name].Add(grade);
+                }
+                else
+                {
+                    grades[name].Add(grade);
+                }
+            }
+
+            foreach (KeyValuePair<string, List<decimal>> student in grades)
+            {
+                // Използваме student.Value.Select(grade => grade.ToString("F2"))) за да форматираме до 2 цифра след десетичната запетая!
+                Console.WriteLine($"{student.Key} -> {string.Join(" ",student.Value.Select(grade => grade.ToString("F2")))} (avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
