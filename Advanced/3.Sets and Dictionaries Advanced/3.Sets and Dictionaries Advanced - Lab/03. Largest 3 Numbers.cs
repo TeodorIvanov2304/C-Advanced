5b6c055001568c208345d9571b12eb987d57bf95
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-16 22:34:51 +0200

    Create 03. Largest 3 Numbers.cs

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers.cs
@@ -0,0 +1,28 @@
+﻿namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine()
+                             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToArray();
+
+            int[] sortedIntegers = integers.OrderByDescending(x => x).ToArray();
+
+            //Алтернатива на break:
+            //int count = numbers.Length >= 3 ? 3 : numbers.Length; - ако дължината е по-малка от 3, count = дължината. После пускаме for-loop до count
+            // for (int i = 0; i < count; i++)
+
+            for (int i = 0; i < 3; i++)
+            {   
+                if (sortedIntegers.Length<3 && i == sortedIntegers.Length)
+                {
+                    break;
+                }
+                Console.Write($"{sortedIntegers[i]} ");
+            }
+        }
+    }
+}
