83e1bf20fe3afe3724a1ec37d1c080e6d29a3e9d
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-17 13:50:22 +0200

    Create 07. Parking Lot.cs

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Lab/07. Parking Lot.cs
@@ -0,0 +1,43 @@
+﻿namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            //Създаваме хешсет
+            HashSet<string> parking = new HashSet<string>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] inputArgs = input.Split(", ");
+                string direciton = inputArgs[0];
+                string carNumber = inputArgs[1];
+
+                //Ако посоката е IN, добавяме кола с функцията Add
+                if (direciton == "IN")
+                {
+                    parking.Add(carNumber);
+                }
+                //Ако е OUT, премахваме кола с Remove
+                else
+                {
+                    parking.Remove(carNumber);
+                }
+
+
+            }
+
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (string car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
