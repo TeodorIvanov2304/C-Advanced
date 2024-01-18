0f847398e992f3a34450cd026f7cdbd3de510bd7
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-18 18:54:57 +0200

    Created 3.Sets and Dictionaries Advanced - Exercise Folder and First Problem

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Exercise/01. Unique Usernames.cs
@@ -0,0 +1,22 @@
+﻿namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> usernames = new HashSet<string>();
+            int users = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < users; i++)
+            {
+                string user = Console.ReadLine();
+                usernames.Add(user);
+            }
+
+            foreach (var user in usernames)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
