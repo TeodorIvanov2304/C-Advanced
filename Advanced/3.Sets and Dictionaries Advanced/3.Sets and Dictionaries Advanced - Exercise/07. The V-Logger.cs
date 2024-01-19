325fd0c3961b5535b5afdcbe2f0dfca49fbee19e
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-19 21:19:30 +0200

    Create 07. The V-Logger.cs

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Exercise/07. The V-Logger.cs
@@ -0,0 +1,62 @@
+﻿
+namespace _07._The_V_Logger
+{
+    class Vlogger
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, HashSet<string>>> vlogerDatabase = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Statistics")
+            {
+                string[] data = input.Split();
+
+                string vlogger = data[0];
+                string command = data[1];
+
+                if (command == "joined")
+                {
+                    if (vlogerDatabase.ContainsKey(vlogger) == false)
+                    {
+                        vlogerDatabase.Add(vlogger, new Dictionary<string, HashSet<string>>());
+                        vlogerDatabase[vlogger].Add("followers", new HashSet<string>());
+                        vlogerDatabase[vlogger].Add("following", new HashSet<string>());
+                    }
+                }
+                else if (command == "followed")
+                {
+                    string member = data[2];
+
+                    if (vlogger != member && vlogerDatabase.ContainsKey(vlogger) && vlogerDatabase.ContainsKey(member))
+                    {
+                        vlogerDatabase[vlogger]["following"].Add(member);
+                        vlogerDatabase[member]["followers"].Add(vlogger);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vlogerDatabase.Count} vloggers in its logs.");
+
+            int number = 1;
+
+            foreach (var vlogger in vlogerDatabase.OrderByDescending(v => v.Value["followers"].Count).ThenBy(v => v.Value["following"].Count))
+            {
+                Console.WriteLine($"{number}. {vlogger.Key} : {vlogger.Value["followers"].Count} followers, {vlogger.Value["following"].Count} following");
+
+                if (number == 1)
+                {
+                    foreach (string follower in vlogger.Value["followers"].OrderBy(f => f))
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+
+                number++;
+            }
+        }
+    }
+}
