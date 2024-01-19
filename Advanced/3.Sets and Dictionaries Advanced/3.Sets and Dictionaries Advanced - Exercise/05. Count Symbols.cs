f131a5da88a0515cc1c2efc8c28999ea2c11d31d
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-19 13:57:34 +0200

    Create 05. Count Symbols.cs

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Exercise/05. Count Symbols.cs
@@ -0,0 +1,30 @@
+﻿namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            SortedDictionary<char, int> occurences = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {   
+                char currentSymbol = text[i];
+                if (occurences.ContainsKey(currentSymbol))
+                {
+                    occurences[currentSymbol]++;
+                }
+                else
+                {
+                    occurences[currentSymbol] = 1;
+                }
+
+            }
+
+            foreach (var pair in occurences)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value} time/s");
+            }
+        }
+    }
+}
