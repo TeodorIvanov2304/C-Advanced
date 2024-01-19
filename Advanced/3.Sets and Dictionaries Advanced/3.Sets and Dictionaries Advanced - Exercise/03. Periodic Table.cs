913e221079a97577427c8bcc38cdc01e32b74a9a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-19 11:49:00 +0200

    Create 03. Periodic Table.cs

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Exercise/03. Periodic Table.cs
@@ -0,0 +1,23 @@
+﻿namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            //Създаваме сортирана хаш-таблица
+            SortedSet<string> periodicTable = new SortedSet<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                foreach (string element in elements) 
+                {
+                    periodicTable.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", periodicTable));
+        }
+    }
+}
