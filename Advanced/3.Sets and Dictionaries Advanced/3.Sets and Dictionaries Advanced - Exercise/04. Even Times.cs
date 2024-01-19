84b0f655e7708c7e521f59b8f9f0b51e8a218c04
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-19 13:57:51 +0200

    Create 04. Even Times.cs

---
+++ Advanced/3.Sets and Dictionaries Advanced/3.Sets and Dictionaries Advanced - Exercise/04. Even Times.cs
@@ -0,0 +1,39 @@
+﻿namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            //Създаваме речник от тип int, bool
+            Dictionary<int, bool> numbers = new Dictionary<int, bool>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                //Целта е да намерим числото, което се съдържа четен брой пъти
+                //Ако текущото число, вече се съдържа в речника, числото с ключ текущо число става обратното на false(true) при две срещания. При три ще стане false, и т.н
+
+                if (numbers.ContainsKey(currentNumber))
+                {
+                    numbers[currentNumber] = !numbers[currentNumber];
+                }
+                //Ако не се съдържа, добавяме int и false
+                else
+                {
+                    numbers.Add(currentNumber, false);
+                }
+            }
+
+            foreach (var number in numbers.Keys)
+            {   
+                //Ако numbers от ключ number = true
+                if (numbers[number])
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
