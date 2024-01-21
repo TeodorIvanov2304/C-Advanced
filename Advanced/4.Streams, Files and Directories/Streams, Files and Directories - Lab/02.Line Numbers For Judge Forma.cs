4aef16412b22da587c0bc62724a460063758b5fd
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-21 20:04:04 +0200

    Create 02.Line Numbers For Judge Forma.cs

---
+++ Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Lab/02.Line Numbers For Judge Forma.cs
@@ -0,0 +1,37 @@
+﻿namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            
+            //Пишем using, и създавем нов StreamReader, като в скобите пишем@$" тук е целият път, + окончанието на файла"
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                
+                //Пишем using, и създаваме StreamWriter, като вместо пътя използваме името на стринга,в който е записан пътя
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int line = 1;
+
+                    //Пускаме while-loop, който върти, докато не даде EndOfStream, т.е стигне до последния ред
+                    while (!reader.EndOfStream)
+                    {
+                        //Запазваме, като вместо Console.ReadLine() пишем writer.ReadLine
+                        writer.WriteLine($"{line++}. {reader.ReadLine()}");
+                    }
+                }
+
+                
+            }
+        }
+    }
+}
