142fa3b183bdeea4475e3911e34a200419b737aa
TeodorIvanov <131561902+TeodorIvanov2304@users.noreply.github.com>
2024-01-21 21:34:36 +0200

    Update 02. Line Numbers.cs

--- Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Lab/02. Line Numbers.cs
+++ Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Lab/02. Line Numbers.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {   
             //Пишем using, и създавем нов StreamReader, като в скобите пишем@$" тук е целият път, + окончанието на файла"
+            //Това е абсолютен път, и използването му обикновено е грешка, защото се чете само на твоят компютър
             using (StreamReader reader = new StreamReader(@$"C:\Users\Garo\Desktop\C#\ReadingFromFiles\READ.txt"))
             {
                 int line = 1;
