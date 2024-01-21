1073ec16c75a5f93e0af59290a3da9a717cbfe27
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-21 20:03:24 +0200

    Create 02. Line Numbers.cs

---
+++ Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Lab/02. Line Numbers.cs
@@ -0,0 +1,20 @@
+﻿namespace _02._Line_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Пишем using, и създавем нов StreamReader, като в скобите пишем@$" тук е целият път, + окончанието на файла"
+            using (StreamReader reader = new StreamReader(@$"C:\Users\Garo\Desktop\C#\ReadingFromFiles\READ.txt"))
+            {
+                int line = 1;
+                //Пускаме while-loop, който върти, докато не даде EndOfStream, т.е стигне до последния ред
+                while (!reader.EndOfStream) 
+                {   
+                    //Изпечатваме с reader.ReadLine();
+                    Console.WriteLine($"{line++}. {reader.ReadLine()}");
+                }
+            }
+        }
+    }
+}
