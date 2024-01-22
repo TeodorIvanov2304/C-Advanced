67685abb27e4eda07c2f90df604d8fb07658d824
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-22 22:23:39 +0200

    Create 01. Even Lines.cs

---
+++ Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Exercises/01. Even Lines.cs
@@ -0,0 +1,55 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            char[] cArray = { '-', ',', '.', '!', '?' };
+            int lineCount = 0;
+            string line;
+            StringBuilder stringBuild = new StringBuilder();
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                
+                
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    if (lineCount % 2 == 0)
+                    {
+                        foreach (char c in line)
+                        {
+                            if (cArray.Contains(c))
+                            {
+                                line = line.Replace(c, '@');
+                                
+                            }
+                        }
+                        var temp = line
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Reverse()
+                                    .ToArray();
+                        stringBuild.AppendLine(string.Join(' ', temp));
+
+
+                    }
+                    lineCount++;
+                   
+                }
+            }
+
+
+            return stringBuild.ToString();
+        }
+    }
+}
