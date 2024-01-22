5ac18396429b15e34f793ef45b2f04a460ed9c16
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-22 14:31:55 +0200

    Create 04. Merge Text Files.cs

---
+++ Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Lab/04. Merge Text Files.cs
@@ -0,0 +1,45 @@
+﻿namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {   
+            //За да четем от 2 файла едновременно, влагаме 2 рийдъра 1 в друг, в вътре в тях Writer
+            using (StreamReader reader = new StreamReader(firstInputFilePath))
+            {
+                using (StreamReader reader2 = new StreamReader(secondInputFilePath))
+                {
+                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    {
+                        while (!reader.EndOfStream || !reader2.EndOfStream)
+                        {   
+                            string reader1Lines = reader.ReadLine();
+                            string reader2Lines = reader2.ReadLine();
+
+                            //Ако реда е празен, не записваме нищо
+                            if (reader1Lines != null)
+                            {
+                                writer.WriteLine(reader1Lines);
+                            }
+                                
+                            if (reader2Lines != null)
+                            {
+                                writer.WriteLine(reader2Lines);
+                            }
+                                
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
