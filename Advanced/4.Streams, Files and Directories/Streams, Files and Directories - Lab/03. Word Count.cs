53519cac98cc6e8c5ea3dbe4a895f1422b878e13
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-22 12:55:29 +0200

    Created two solutions

---
+++ Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Lab/03. Word Count.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            ReadWordsToFind(wordsFilePath, words);
+
+            using (StreamReader reader = new StreamReader(textFilePath))
+            {
+                ReadTextAndMatch(words, reader);
+
+                var sortedWords = words.OrderByDescending(x => x.Value);
+                WriteWordsInFile(outputFilePath, sortedWords);
+
+            }
+        }
+
+        private static void WriteWordsInFile(string outputFilePath, IOrderedEnumerable<KeyValuePair<string, int>> sortedWords)
+        {
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (KeyValuePair<string, int> word in sortedWords)
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+
+        private static void ReadTextAndMatch(Dictionary<string, int> words, StreamReader reader)
+        {
+            while (!reader.EndOfStream)
+            {
+                string pattern = @"\w{1,}";
+                string text = reader.ReadLine().ToLower();
+                MatchCollection matches = Regex.Matches(text, pattern);
+                MatchWordsAndAddToDictionary(words, matches);
+            }
+        }
+
+        private static void MatchWordsAndAddToDictionary(Dictionary<string, int> words, MatchCollection matches)
+        {
+            foreach (Match item in matches)
+            {
+
+                if (words.ContainsKey(item.ToString()))
+                {
+                    words[item.ToString()]++;
+                }
+            }
+        }
+
+        private static void ReadWordsToFind(string wordsFilePath, Dictionary<string, int> words)
+        {
+            using (StreamReader reader = new StreamReader(wordsFilePath))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    string[] word = reader.ReadLine().Split();
+
+                    foreach (var item in word)
+                    {
+                        words.Add(item, 0);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Lab/07. Folder Size Recursion.cs
@@ -0,0 +1,58 @@
+﻿namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {   
+            //Записваме пътя на тестовата папка и файла, в който ще записваме
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            //Викаме функцията, която ще смята размера и й подаваме folderPath, outputPath
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        //Функция, която намира размера на папката
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {   
+            //long променлива siez, коят вика функцията, четяща папките
+            long size = ReadFolder(folderPath);
+
+            //Създаваме StreamWriter и изпечатваме с него, /1024 за да намерим размера в кб
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                writer.WriteLine($"{size / 1024.0}kb");
+            }
+        }
+
+        //Функция, която чете папките и смята размера им
+        public static long ReadFolder(string folderPath, int levels = 0)
+        {
+            //Създаваме масив от файлове, който с методите Directory.GetFiles(folderPath) взима колкото файла има във folderPath
+            string[] files = Directory.GetFiles(folderPath);
+            //Променлива от тип лонг, която ще събира размерите
+            long size = 0;
+            //За всеки файл, в масива файлс
+            foreach (string file in files) 
+            {   
+                //Извикваме класа Fileinfo, и създаваме инстанция към него, като й подаваме file
+                FileInfo fileInfo = new FileInfo(file);
+                //Добавяме размера на инстанцията, която се намира с fileInfo.Length;
+                size += fileInfo.Length;
+
+            }
+
+            //Масив директории, извикваме класа Directory, и метода му GetDirectories, който връща масив от поддиректории от folderPath
+            string[] directories = Directory.GetDirectories(folderPath);
+
+            //За всяка директория директории
+            foreach (var dir in directories)
+            {   
+                //Добавяме към общите kb размера на текущата директория и ?ниво + 1?
+                size += ReadFolder(dir, levels + 1);
+            }
+
+            return size;
+        }
+    }
+}
