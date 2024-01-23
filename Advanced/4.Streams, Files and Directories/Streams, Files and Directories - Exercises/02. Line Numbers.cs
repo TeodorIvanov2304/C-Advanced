d0cea77b3488a062c836a3ed5a8e55cc1bd19f45
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-23 11:57:07 +0200

    Create 02. Line Numbers.cs

---
+++ Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Exercises/02. Line Numbers.cs
@@ -0,0 +1,33 @@
+﻿namespace LineNumbers
+{
+    using System;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            //Четем всичко наведнъж с File.ReadAllLines(inputFilePath);
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                
+                //С метода where, се казва където, char.IsLetter всичко което е букв, Count() изброй
+                int lettersNumber = lines[i].Where(char.IsLetter).Count();
+                //По същия начин, но където е пунктуация
+                int symbolsNumber = lines[i].Where(char.IsPunctuation).Count();
+                lines[i] = @$"Line {i + 1}: {lines[i]} ({lettersNumber}) ({symbolsNumber})";
+            }
+
+            //Записваме във файл, като първо подаваме къде да се запише, после какво (lines)
+            File.WriteAllLines(outputFilePath,lines);
+        }
+    }
+}
