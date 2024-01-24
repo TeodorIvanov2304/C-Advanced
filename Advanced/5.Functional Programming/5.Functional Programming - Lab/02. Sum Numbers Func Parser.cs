12cc16b0f20e4feb6115bfa6f4b570c22a57a9c2
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-24 21:47:23 +0200

    Create 02. Sum Numbers Func Parser.cs

---
+++ Advanced/5.Functional Programming/5.Functional Programming - Lab/02. Sum Numbers Func Parser.cs
@@ -0,0 +1,21 @@
+﻿namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            //Създаваме функция parser, която приема string и връща int. Правим ламбда функция n=> int.Parse(n)
+            //После вместо всеки път да пишем int.Parse пишем само parser
+            Func<string, int> parser = n => int.Parse(n);
+
+            int[] numbers = input.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(parser)
+                                 .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
