c00f12eef716018bc357d996bd00a66c75b790fb
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-24 15:26:10 +0200

    Created Functional Programming folder and first solution

---
+++ Advanced/5.Functional Programming/5.Functional Programming - Lab/01. Sort Even Numbers.cs
@@ -0,0 +1,19 @@
+﻿namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .Where(n => n % 2 == 0) //Където n е четно число
+                            .OrderBy(n => n)        //Подреждаме във възходящ ред
+                            .ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
+
+
+        }
+    }
+}
