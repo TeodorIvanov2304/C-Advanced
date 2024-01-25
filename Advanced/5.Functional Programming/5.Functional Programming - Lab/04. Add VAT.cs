757884be91326dd7ccbc0467c0a702d7546de906
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-25 18:56:19 +0200

    Create 04. Add VAT.cs

---
+++ Advanced/5.Functional Programming/5.Functional Programming - Lab/04. Add VAT.cs
@@ -0,0 +1,24 @@
+﻿namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, double> parser = n => double.Parse(n);
+            //Създаваме предикат, който ще добави 20% към числата
+            Func<double, double> vat = p => p * 1.20;
+            double[] products = Console.ReadLine()
+                                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(parser)
+                                .Select(vat) //Правим още един Select, и му vat предикатът
+                                .ToArray();
+
+            
+            foreach (var product in products) 
+            {
+                Console.WriteLine($"{product:F2}");
+            }
+
+        }
+    }
+}
