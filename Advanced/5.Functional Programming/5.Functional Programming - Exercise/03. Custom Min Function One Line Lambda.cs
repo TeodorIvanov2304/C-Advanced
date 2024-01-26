7f0106adc6e7e21d570152b037fa0709869aa943
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-26 14:41:03 +0200

    Created 2 solutions

---
+++ Advanced/5.Functional Programming/5.Functional Programming - Exercise/03. Custom Min Function One Line Lambda.cs
@@ -0,0 +1,20 @@
+﻿namespace _03._Custom_Min_Function_One_Line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .ToArray();
+            //Find min
+            Func<int[], int> customMin = arr => arr.Min();
+            //Find max
+            Func<int[], int> customMax = arr => arr.Max();
+            Console.WriteLine(customMin(numbers));
+            Console.WriteLine(customMax(numbers));
+        }
+    }
+}
---
+++ Advanced/5.Functional Programming/5.Functional Programming - Exercise/03. Custom Min Function.cs
@@ -0,0 +1,40 @@
+﻿namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int[] numbers = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .ToArray();
+
+            //Правим проверка, дали масива е празен
+            if (numbers.Any() == false)
+            {
+                return;
+            }
+
+            //Ламбда функция, но на повече от 1 ред. Приема int[], и връща int
+            Func<int[], int> minimum = (int[] input) =>
+            {
+
+                int smallest = input[0];
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] < smallest)
+                    {
+                        smallest = input[i];
+                    }
+                }
+                return smallest;
+            };
+
+            Console.WriteLine(minimum(numbers));
+        }
+
+        
+    }
+}
