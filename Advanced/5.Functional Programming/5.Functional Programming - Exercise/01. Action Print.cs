9e28db4c2b522e6621f6a1ffc1df43cd55039c16
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-26 14:00:23 +0200

    Created Folder and Problem 1 solution

---
+++ Advanced/5.Functional Programming/5.Functional Programming - Exercise/01. Action Print.cs
@@ -0,0 +1,25 @@
+﻿namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Action is allways void
+            Action<string> print = name => Console.WriteLine(name);
+            string[] names = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            //Използваме клас Array и метода Foreach, като за параметри му подаваме масива, и анонимната ламбда функция Action<string> print
+            Array.ForEach(names, print);
+
+            //Алтернативно изписване, + метода Print. Да се обърне внимание, че не се слагат () след принт, когато той е референция!
+            //Action<string> print2 = Print;
+            //Array.ForEach(names, print2);
+        }
+
+        static void Print(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
