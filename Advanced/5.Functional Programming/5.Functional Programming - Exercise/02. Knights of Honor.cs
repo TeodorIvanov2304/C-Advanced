79245edb3931652b6ee555b1918b6375ccf4884c
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-26 14:06:35 +0200

    Create 02. Knights of Honor.cs

---
+++ Advanced/5.Functional Programming/5.Functional Programming - Exercise/02. Knights of Honor.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Channels;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> honor = name => Console.WriteLine($"Sir {name}");
+            Array.ForEach(names, honor);
+
+            //Equivalent for the previous lines + Method
+            //Action<string> honor2 = PrintHonor;
+            //Array.ForEach(names, honor2);
+        }
+
+        //static void PrintHonor(string name)
+        //{
+        //    Console.WriteLine($"Sir {name}");
+        //}
+    }
+}
