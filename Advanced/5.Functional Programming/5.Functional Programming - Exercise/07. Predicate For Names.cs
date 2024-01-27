ba1e1fe3b993f125621b5d2f1dab38075b5f06d6
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-27 12:49:45 +0200

    Create 07. Predicate For Names.cs

---
+++ Advanced/5.Functional Programming/5.Functional Programming - Exercise/07. Predicate For Names.cs
@@ -0,0 +1,17 @@
+﻿namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Predicate<string> predicate = x => x.Length <= length;
+
+            string[] sorted = names.Where(n=>predicate(n)).ToArray();
+            Console.WriteLine(string.Join("\n",sorted));
+        }
+    }
+}
