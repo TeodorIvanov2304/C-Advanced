8f40781de3aa1aa981211c90c3004d2e8102ba26
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-27 13:07:36 +0200

    Update 07. Predicate For Names.cs

--- Advanced/5.Functional Programming/5.Functional Programming - Exercise/07. Predicate For Names.cs
+++ Advanced/5.Functional Programming/5.Functional Programming - Exercise/07. Predicate For Names.cs
@@ -8,8 +8,10 @@
             string[] names = Console.ReadLine()
                                     .Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
+            
             Predicate<string> predicate = x => x.Length <= length;
-
+            //Ако се направи с предикат, няма да е чиста функция. Ако се използва повече от една променлива, се използва Func, като параметрите се слгат в скоби, както по-долу
+            //Func<string,int,bool> predicate2 = (x,i) => x.Length <= length;
             string[] sorted = names.Where(n=>predicate(n)).ToArray();
             Console.WriteLine(string.Join("\n",sorted));
         }
