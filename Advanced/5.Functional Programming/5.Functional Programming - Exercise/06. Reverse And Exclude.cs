17f7eaa925b872f3de66f4f4f83047a0c00bf7a4
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-27 12:35:01 +0200

    Create 06. Reverse And Exclude.cs

---
+++ Advanced/5.Functional Programming/5.Functional Programming - Exercise/06. Reverse And Exclude.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .Reverse()
+                                 .ToArray();
+
+            int divisor = int.Parse(Console.ReadLine());
+
+            Predicate<int> predicate = n => n % divisor != 0;
+
+            input = input.Where(n =>predicate(n)).ToArray();
+
+            Console.WriteLine(string.Join(' ', input));
+
+        }
+    }
+}
