c6490d325400efbaf40c76d560aa285c0e984d29
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-29 16:13:46 +0200

    Created Folder and 03. Car Constructors problem

---
+++ Advanced/6.Defining Classes/6.Defining Classes - Lab/03. Car Constructors/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car(); // First Constructor
+            Car secondCar = new Car(make, model, year); //Second Constructor
+            Car thirdCar = new Car(make,model,year,fuelQuantity,fuelConsumption); //Third
+        }
+    }
+}
