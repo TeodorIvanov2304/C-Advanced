37a8959b4e1e71dd3dc7de189562c65f567fdade
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-29 16:57:15 +0200

    Created Folder 04. Car Engine And Tires and 4 files with code

---
+++ Advanced/6.Defining Classes/6.Defining Classes - Lab/04. Car Engine And Tires/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {   
+            //Когато създаваме масив от Tire, трябва да кажем new на всяка една от гумите, и да им подадем year и pressure
+            Tire[] tires = new Tire[4]
+            {
+                new Tire (1,2.5),
+                new Tire (1,2.1),
+                new Tire (2,0.5),
+                new Tire (2,2.3)
+            };
+
+            Engine engine = new(560, 6300);
+
+            //engine, tires вече са сетнатти, за това не им подаваме стойност, като напр. "Urus"
+            Car car = new("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+        }
+    }
+}
