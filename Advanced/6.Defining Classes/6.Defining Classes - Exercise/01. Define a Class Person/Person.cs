0efe505779352a76a71088a1b37b491714bc3e60
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-30 20:03:17 +0200

    Created folder and 1 problem solution

---
+++ Advanced/6.Defining Classes/6.Defining Classes - Exercise/01. Define a Class Person/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private uint age;
+
+        public string Name
+        {   
+            //get=>name е алтернативно изписване
+            //set=>name = value
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public uint Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+}
---
+++ Advanced/6.Defining Classes/6.Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person peter = new Person();
+            peter.Name = "Peter";
+            peter.Age = 20;
+            Person george = new Person();
+            george.Name = "George";
+            george.Age = 18;
+            Person jose = new();
+            jose.Name = "Jose";
+            jose.Age = 43;
+
+        }
+    }
+}
