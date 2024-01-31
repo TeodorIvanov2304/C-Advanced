1b31c2b6b7d8dc70f76a0a689f720508e3fcdcb5
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-31 14:18:15 +0200

    Created 04. Opinion Poll folder and solution

---
+++ Advanced/6.Defining Classes/6.Defining Classes - Exercise/04. Opinion Poll/Family.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> family = new List<Person>();
+        public Family()
+        {
+            this.family = new();
+        }
+
+        public List<Person> FamilyMembers
+        {
+            get { return family; }
+            set { family = value; }
+        }
+
+
+        public void AddMember(Person person)
+        {
+            family.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldest = family.MaxBy(x => x.Age);
+            return oldest;
+        }
+    }
+}
---
+++ Advanced/6.Defining Classes/6.Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using DefiningClasses;
+
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<Person> persons = new List<Person>();
+        for (int i = 0; i < n; i++)
+        {
+            string[] currentPerson = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = currentPerson[0];
+            int age = int.Parse(currentPerson[1]);
+
+            Person person = new Person(name, age);
+
+            persons.Add(person);
+        }
+
+        List<Person> personsMoreThan30 = persons.OrderBy(person => person.Name).ThenBy(p=>p.Age).ToList();
+
+        foreach (var person in personsMoreThan30.Where(p=>p.Age > 30))
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+    }
+}
