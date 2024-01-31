faeedb15998730f4276d105af7530d237b15ea27
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-31 15:47:39 +0200

    Created Folder 05. Date Modifier and solution

---
+++ Advanced/6.Defining Classes/6.Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,31 @@
+﻿namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        private int difference;
+
+        private DateTime firstDate;
+        private DateTime secondDate;
+
+        //Конструктора получава два стринга и ги връща парснати
+        public DateModifier(string firstDate, string secondDate)
+        {
+            //Получаваме string firstDate и после го парсваме към DateTime
+            this.firstDate = DateTime.Parse(firstDate);
+            this.secondDate = DateTime.Parse(secondDate);
+
+        }
+
+        public int Difference
+        {
+            get
+            {   
+                //В гетъра направо кастваме firstDate - secondDate към int, и след това му даваме .TotalDays
+                return difference = Math.Abs((int)(firstDate - secondDate).TotalDays);
+            }
+            set { this.difference = value; }
+        }
+
+
+    }
+}
---
+++ Advanced/6.Defining Classes/6.Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        string firstDate = Console.ReadLine();
+        string secondDate = Console.ReadLine();
+        DateModifier dateModifier = new DateModifier(firstDate, secondDate);
+        //Изпечатваме property-то на dateModifier
+        Console.WriteLine(dateModifier.Difference);
+    }
+}
