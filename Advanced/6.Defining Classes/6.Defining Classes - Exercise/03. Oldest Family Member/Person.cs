cfdbfaef6ecb6ef900dc25b9ede1dccf6d2012f9
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-01-31 13:46:43 +0200

    Created Folder 03. Oldest Family Member and solution

---
+++ Advanced/6.Defining Classes/6.Defining Classes - Exercise/03. Oldest Family Member/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age) : this(age)
+        {
+            Name = name;
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public Family Family { get; set; }
+    }
+}
---
+++ Advanced/6.Defining Classes/6.Defining Classes - Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using DefiningClasses;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Family family = new Family();
+        for (int i = 0; i < n; i++)
+        {
+            string[] currentPerson = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = currentPerson[0];
+            int age = int.Parse(currentPerson[1]);
+
+            Person person = new Person(name,age);
+            
+            family.AddMember(person);
+        }
+
+        Person oldestPerson = family.GetOldestMember();
+        Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+    }
+}
