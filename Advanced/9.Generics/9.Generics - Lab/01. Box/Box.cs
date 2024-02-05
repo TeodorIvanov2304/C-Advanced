c8bf484f8d6b8bcaea16f309f686b77019cf548d
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-05 15:25:32 +0200

    Create Folder Generics and solution

---
+++ Advanced/9.Generics/9.Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,25 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> box;
+
+        
+        public Box()
+        {
+            this.box = new Stack<T>();
+        }
+
+        public int Count { get {return box.Count; } }
+
+        public void Add(T element)
+        {
+            box.Push(element);
+        }
+
+        public T Remove()
+        {
+            return box.Pop();
+        }
+    }
+}
