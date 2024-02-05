d6b41da7c5db9aabc188e899b447cd0ffed9cf7a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-05 16:38:23 +0200

    Created Generic Scale Folder and solution

---
+++ Advanced/9.Generics/9.Generics - Lab/03.Generic Scale/EqualityScale.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public bool AreEqual() 
+        {   
+            //Използваме .Equals, което директно сравнява обекти
+            return left.Equals(right);
+        }
+    }
+}
---
+++ Advanced/9.Generics/9.Generics - Lab/03.Generic Scale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {   
+            //Директно подаваме двете числа 5 и 6, защото конструктора ги изисква
+            EqualityScale<int> scale = new(5,6);
+            //Използваме създадения метод AreEqual
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
