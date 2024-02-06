8481fffb7f91bca610094ddf0263cab158bf17e5
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-06 17:45:17 +0200

    Create Generate Swap Method Integer

---
+++ Advanced/9.Generics/9.Generics - Exercise/04.Generic Swap Method Integer/Box.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        private List<T> _list;
+
+        public Box()
+        {
+            this._list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _list.Add(item);
+        }
+
+        public void Swap<T>(List<T> list, int index1, int index2)
+        {
+            if (index1 < 0 || index2 < 0 || index1 > list.Count - 1 || index1 > list.Count - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+
+
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (T item in _list)
+            {
+                stringBuilder.AppendLine($"{typeof(T)}: {item}");
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Advanced/9.Generics/9.Generics - Exercise/04.Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace GenericSwapMethodInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<int> boxes = new List<int>();
+            for (int i = 0; i < lines; i++)
+            {
+                boxes.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Swap(boxes, indexes[0], indexes[1]);
+            foreach (var box in boxes)
+            {
+                //box.GetType() действа като typeof, връща ни типа
+                Console.WriteLine($"{box.GetType()}: {box}");
+            }
+
+        }
+
+        public static void Swap<T>(List<T> list, int index1, int index2)
+        {
+            if (index1 < 0 || index2 < 0 || index1 > list.Count - 1 || index1 > list.Count - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+            //Tupple, deconstruciton syntax
+            //(items[index1],items[index2]) = (items[index2],items[index1])
+
+        }
+    }
+}
