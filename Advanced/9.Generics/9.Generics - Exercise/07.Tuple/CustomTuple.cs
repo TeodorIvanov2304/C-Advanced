57ad27c98777a8d73e30dfac2cb7967d804b4992
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-07 01:28:18 +0200

    Created 07. Tuple Example

---
+++ Advanced/9.Generics/9.Generics - Exercise/07.Tuple/CustomTuple.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Tuple
+{   
+    //Създаваме custom class, като му задаваме два типа данни T1, T2
+    public class CustomTuple<T1,T2>
+    {
+        public CustomTuple(T1 item1, T2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+
+        public override string ToString()
+        {   
+            return $"{Item1} -> {Item2}";
+        }
+    }
+}
---
+++ Advanced/9.Generics/9.Generics - Exercise/07.Tuple/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] namesTokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<string, string> tuple = new Tuple.CustomTuple<string, string>(namesTokens[0]+" " + namesTokens[1], namesTokens[2]);
+            Console.WriteLine(tuple);
+
+            string[] beerTokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<string,int> secondTuple = new CustomTuple<string, int>(beerTokens[0], int.Parse(beerTokens[1]));
+            Console.WriteLine(secondTuple);
+
+            string[] numbersToken = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<int,double>  thirdTuple = new CustomTuple<int, double>(int.Parse(numbersToken[0]), double.Parse(numbersToken[1]));
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
