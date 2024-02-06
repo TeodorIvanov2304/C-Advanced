bf921ef747b5f44c0b64e315a14e4b0bbc723a2e
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-06 22:04:16 +0200

    Create 06. Generic Count Method Double

---
+++ Advanced/9.Generics/9.Generics - Exercise/06.Generic Count Method Double/Box.cs
@@ -0,0 +1,31 @@
+﻿namespace GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable
+    {
+        private List<T> _list;
+
+        public Box()
+        {
+            this._list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _list.Add(item);
+        }
+
+        public int CountBiggerValues(T element)
+        {
+            int count = 0;
+            foreach (var item in _list)
+            {
+                //Ако сравнението между item и елемент > 0, значи
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Advanced/9.Generics/9.Generics - Exercise/06.Generic Count Method Double/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Box<double> list = new Box<double>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                list.Add(double.Parse(Console.ReadLine()));
+            }
+
+            double elementForComparsion = double.Parse(Console.ReadLine());
+            Console.WriteLine(list.CountBiggerValues(elementForComparsion));
+        }
+    }
+}
