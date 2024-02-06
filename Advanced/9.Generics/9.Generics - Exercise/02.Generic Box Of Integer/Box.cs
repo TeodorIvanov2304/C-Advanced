864b356bd4b2b28dd8a4f2761267dd73549eb4c6
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-06 12:13:10 +0200

    Created Generic Box Of Integer Solution and folder

---
+++ Advanced/9.Generics/9.Generics - Exercise/02.Generic Box Of Integer/Box.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace BoxOfInteger
+{
+    public class Box<T>
+    {
+        private List<T> _list;
+
+        public Box()
+        {
+            this._list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _list.Add(item);
+        }
+
+        public override string ToString()
+        {   
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (T item in _list) 
+            {
+                stringBuilder.AppendLine($"{typeof(T)}: {item}");
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Advanced/9.Generics/9.Generics - Exercise/02.Generic Box Of Integer/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace BoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < lines; i++)
+            {
+                box.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}
