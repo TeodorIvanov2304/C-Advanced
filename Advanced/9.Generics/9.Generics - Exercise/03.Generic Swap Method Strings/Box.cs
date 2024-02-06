ec547fbf61a276d3cb1b024e9c1b307367120ab7
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-06 17:04:29 +0200

    Created 03. Generic Swap Method String

---
+++ Advanced/9.Generics/9.Generics - Exercise/03.Generic Swap Method Strings/Box.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    public class Box<T>
+    {
+        private List<T> _list;
+
+        public Box()
+        {
+            this._list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _list.Add(item);
+        }
+
+        
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (T item in _list)
+            {
+                stringBuilder.AppendLine($"{typeof(T)}: {item}");
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
