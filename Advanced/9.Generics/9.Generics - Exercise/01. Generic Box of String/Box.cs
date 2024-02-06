5df38c25c3b22cddda06ed50808052ec2fd9e1bc
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-06 12:00:32 +0200

    Created Generics Exercise folder and 01. Box of String

---
+++ Advanced/9.Generics/9.Generics - Exercise/01. Generic Box of String/Box.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace BoxOfString
+{
+    public class Box<T>
+    {
+        private List<T> _boxes;
+        public Box()
+        {
+            this._boxes = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _boxes.Add(item);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T item in _boxes) 
+            {   
+                //Typeof изкарва типа на променливата
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Advanced/9.Generics/9.Generics - Exercise/01. Generic Box of String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace BoxOfString
+
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Box<string> stringBox = new Box<string>();
+            for (int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+                stringBox.Add(input);
+            }
+
+            Console.WriteLine(stringBox);
+        }
+    }
+}
