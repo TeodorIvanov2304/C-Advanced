ae2afa91c4d5f57b456cc4b30e865684034fef46
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-06 21:54:51 +0200

    Create 05. Count Method String

---
+++ Advanced/9.Generics/9.Generics - Exercise/05.Generic Count Method String/Box.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace GenericCountMethodString
+{   
+    //С where T: IComparable добавяме ограничители, който указват какъв тип може да е T. В случая избираме
+    //IComparable, т.е типове които може да се сравняват
+    public class Box<T> where T: IComparable
+    {
+        private List<T> _list;
+        private int _count;
+        public Box()
+        {
+            this._list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _list.Add(item);
+        }
+
+        public void Swap<T>(List<T> list, int index1, int index2)
+        {
+            if (index1 < 0 || index2 < 0 || index1 > list.Count - 1 || index1 > list.Count - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+
+
+        }
+        public int Counts(T element)
+        {
+            int count = 0;
+            foreach (var item in _list)
+            {   
+                //Ако сравнението между item и елемент > 0, значи
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (T item in _list)
+            {
+                stringBuilder.AppendLine($"{typeof(T)}: {item}");
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Advanced/9.Generics/9.Generics - Exercise/05.Generic Count Method String/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Box<string> list = new Box<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+            string elementForComparsion = Console.ReadLine();
+            int count = list.Counts(elementForComparsion);
+            Console.WriteLine(count);
+            //Съкратено
+            //Console.WriteLine(list.Counts(elementForComparsion));
+        }
+    }
+}
