580944d633964faffd8adc04b7582ed6eb40968c
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-07 19:43:53 +0200

    Created 08. Threeuple

---
+++ Advanced/9.Generics/9.Generics - Exercise/08.Threeuple/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] namesTokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            
+            string[] beerTokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] bankTokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Threeuple<string, string, string> names = new($"{namesTokens[0]} {namesTokens[1]}", namesTokens[2], namesTokens[3]);
+            Threeuple<string, int, bool> beer = new(beerTokens[0], int.Parse(beerTokens[1]), beerTokens[2] == "drunk");
+            Threeuple<string, double, string> bank = new(bankTokens[0], double.Parse(bankTokens[1]), bankTokens[2]);
+
+            Console.WriteLine(names);
+            Console.WriteLine(beer);
+            Console.WriteLine(bank);
+
+
+        }
+    }
+}
---
+++ Advanced/9.Generics/9.Generics - Exercise/08.Threeuple/Threeuple.cs
@@ -0,0 +1,21 @@
+﻿namespace Threeuple
+{
+    public class Threeuple<TItem1, TItem2, TItem3>
+    {
+        public TItem1 Item1 { get; set; }
+        public TItem2 Item2 { get; set; }
+
+        public TItem3 Item3 { get; set; }
+
+        public Threeuple(TItem1 item1, TItem2 item2, TItem3 item3)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+        }
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2} -> {Item3}";
+        }
+    }
+}
