50431283f2bf3bdc0c3b465f022db97b89d3f31d
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-08 15:42:51 +0200

    Created Iterators and Comparators folder and 01. Library solution

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Lab/01.Library/Library.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {   
+            //Използваме вграденият метод на List<T>, GetEnumerator()
+            return books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
