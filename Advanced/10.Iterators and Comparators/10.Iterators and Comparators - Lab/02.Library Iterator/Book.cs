6b2f1e7a7cf677762aec34a11a2889f1c81cdcbb
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-08 16:16:40 +0200

    Created Library Iterator

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Lab/02.Library Iterator/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace IteratorsAndComparators
+{
+    public class Book
+    {   
+        //Използваме params, когато не знаем колко точно променливи ще получим
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+    }
+}
---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Lab/02.Library Iterator/Library.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {   
+            //Използваме вграденият метод на List<T>, GetEnumerator()
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private  int currentIndex = -1;
+            private readonly List<Book> books;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+            }
+
+            public Book Current => throw new NotImplementedException();
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {   
+                //Връщай true, докато не е е достигната гранциата на масива
+                return ++currentIndex<books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+    }
+}
