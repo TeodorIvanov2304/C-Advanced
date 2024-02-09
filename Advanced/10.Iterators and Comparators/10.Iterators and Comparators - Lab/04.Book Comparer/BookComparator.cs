e17a0c1df41efbfbf1cf731d8762f847646861c0
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-09 13:58:14 +0200

    Created 04. Book Comparer

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Lab/04.Book Comparer/BookComparator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book? x, Book? y)
+        {
+            //alphabeticalComparison е от тип инт, защото CompareTo връща -1,0 или 1!
+            int alphabeticalComparison = x.Title.CompareTo(y.Title);
+
+            if (alphabeticalComparison == 0)
+            {
+                return y.Year.CompareTo(x.Year);
+            }
+
+            return alphabeticalComparison;
+        }
+    }
+}
---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Lab/04.Book Comparer/Library.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            //Подаваме новия BookComparator, вместо да използваме default OrderBy
+            return new LibraryIterator(books.OrderBy(b=>b,new BookComparator()).ToList());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+                this.books.Sort();
+            }
+
+            private readonly List<Book> books;
+            private int currentIndex;
+
+            public bool MoveNext()
+            {
+                return ++this.currentIndex < this.books.Count;
+            }
+
+            public void Reset() => this.currentIndex = -1;
+
+
+            public Book Current => this.books[this.currentIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose() { }
+        }
+    }
+}
