6532e4cffe3268a28319526130c0b338b47e3783
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-09 13:41:38 +0200

    Created 03. Comparable Book

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Lab/03.Comparable Book/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        //Използваме params, когато не знаем колко променливи ще получим
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+
+        public int CompareTo(Book other)
+        {
+            if (Year != other.Year)
+            {
+                return Year.CompareTo(other.Year);
+            }
+
+            return Title.CompareTo(other.Title);
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
