da5faf25a28c728bcadbd8ab8e3251b5bea53da9
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-10 16:15:17 +0200

    Created 06. Equality Logic

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/06.Equality Logic/Person.cs
@@ -0,0 +1,49 @@
+﻿namespace EqualityLogic
+{
+    public class Person :IComparable<Person>
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            this._name = name;
+            this._age = age;
+        }
+
+        public string Name
+        {
+            get => _name;
+            private set =>_name = value;
+        }
+        public int Age
+        {
+            get => _age;
+            private set => _age = value;
+        }
+
+        public int CompareTo(Person? other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if(result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Age.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {   
+            Person other = obj as Person;
+            if (other == null)
+            {
+                return false;
+            }
+            return this.Name == other.Name && this.Age == other.Age;
+        }
+    }
+}
---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/06.Equality Logic/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+            HashSet<Person> hashSet = new HashSet<Person>();
+
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Person newPerson = new(tokens[0], int.Parse(tokens[1]));
+                sortedSet.Add(newPerson);
+                hashSet.Add(newPerson);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
