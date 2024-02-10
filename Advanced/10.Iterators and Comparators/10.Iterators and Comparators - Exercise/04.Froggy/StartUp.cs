a1cdee72ddb8236d3afa179720454af22b03aa24
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-10 13:58:27 +0200

    Created 04. Froggy

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/04.Froggy/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                                    .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToList();
+            Lake lake = new(list);
+            string result = string.Join(", ", lake);
+            Console.WriteLine(result);
+        }
+    }
+}
