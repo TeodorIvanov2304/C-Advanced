8f43beeb3294f719f21fe4764f618f40a1ac9593
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-10 11:52:59 +0200

    Created new folder and 01. Listy Iterator

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/01.Listy Iterator/ListyIterator.cs
@@ -0,0 +1,37 @@
+﻿using static System.Reflection.Metadata.BlobBuilder;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private  int _index = 0;
+        private List<T> list;
+        public ListyIterator(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public bool Move()
+        {   
+            if (HasNext()) 
+            {
+                _index++;
+                return true;
+            }
+            return false;
+        }
+        public bool HasNext()
+        {
+            return _index < list.Count-1;
+        }
+
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            Console.WriteLine(list[_index]);
+        }
+    }
+}
---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/01.Listy Iterator/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                           .Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            //Директно подаваме на listy стойностите, като скипваме командата
+            ListyIterator<string> listy = new ListyIterator<string>(input.Skip(1).ToList());
+            while ((input[0] = Console.ReadLine()) != "END")
+            {
+                switch (input[0])
+                {
+                    case "HasNext":
+                        //Направо изпечатваме true / false
+                        Console.WriteLine(listy.HasNext());
+                        break;
+                    case "Move":
+                        Console.WriteLine(listy.Move());
+                        break;
+                    case "Print":
+                        //Пробваме да изпринтим, ако не стане, хвърляме exception
+                        try
+                        {
+                            listy.Print();
+                        }
+                        catch (InvalidOperationException ioe)
+                        {
+
+                            Console.WriteLine("Invalid Operation!");
+                        }
+                        break;
+                    
+                }
+            }
+        }
+    }
+}
