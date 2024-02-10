b8c737469aab7c2e17c7fe08bd604f64a8d0bc21
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-10 15:32:59 +0200

    Created 05. Comparing Objects

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/05.Comparing Objects/Person.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace ComparingObjects
+{
+    public class Person:IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public int CompareTo(Person? other)
+        {
+            //Създаваме променлива,в която ще пазим резултата. После директно й присвояваме стойността от първото сравнение, по град
+            int result = this.Name.CompareTo(other.Name);
+            if(result !=0)
+            {
+                return result;
+            }
+            //Ако първите са равни, презаписваме стойността на второто сравнение
+            result = this.Age.CompareTo(other.Age);
+            if(result !=0) 
+            {
+                return result;
+            }
+
+            //Ако пак са равни, направо връщаме резултата от третото сравнение
+            return this.Town.CompareTo(other.Town);
+        }
+    }
+}
---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/05.Comparing Objects/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            string input = "";
+            while ((input = Console.ReadLine()) != "END") 
+            {
+                string[] inputArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Person person = new(inputArgs[0], int.Parse(inputArgs[1]), inputArgs[2]);
+                persons.Add(person);
+            }
+
+            //Тук се пази индекса на човека, по който ще сравняваме другите хора. Вадим едно от индекса, защото по условие хората трябва да започват от 1
+            int referenceIndex = int.Parse(Console.ReadLine()) -1 ;
+            Person reference = persons[referenceIndex];
+            int equalCount = 0;
+            int differentCount = 0;
+
+            foreach (var person in persons)
+            {
+                if (person.CompareTo(reference) == 0)
+                {
+                    equalCount++;
+                }
+                else 
+                {
+                    differentCount++;
+                }
+            }
+
+            if (equalCount == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equalCount} {differentCount} {persons.Count}");
+            }
+        }
+    }
+}
