515e728c9f30e41a4f7b8bc7c1e35b327b21942a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-10 13:30:06 +0200

    Created 03. Stack

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/03.Stack/Stack.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+
+namespace Stack
+
+{
+    public class CustomStack<T>: IEnumerable<T>
+    {
+        private const int initialCapacity = 4;
+        private T[] _stack;
+        public int Count { get; private set; }
+        public CustomStack()
+        {
+            this._stack = new T[initialCapacity];
+        }
+
+        //Този ForEach метод приема някакво действие, което не връща резултат. Например Console.Writeline();
+        //Действието се подава от Program.cs
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                action(_stack[i]);
+            }
+        }
+        public void Push(T element)
+        {
+            if (Count == _stack.Length)
+            {
+                IncreaseCapacity();
+            }
+            else
+            {
+                _stack[Count] = element;
+            }
+            Count++;
+        }
+
+
+
+        public T Pop()
+        {
+            CheckIfCountIsZero();
+            T result = _stack[Count - 1];
+            Count--;
+
+            return result;
+        }
+
+        private void Resize()
+        {
+            T[] temp = new T[initialCapacity*2];
+            for (int i = 0; i < _stack.Length; i++)
+            {
+                temp[i] = _stack[i];
+            }
+            _stack = temp;
+        }
+
+        public T Peek()
+        {
+            CheckIfCountIsZero();
+
+            return _stack[Count - 1];
+        }
+
+        private void IncreaseCapacity()
+        {
+            T[] copy = new T[_stack.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = _stack[i];
+            }
+            _stack = copy;
+        }
+
+        private void CheckIfCountIsZero()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return _stack[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/03.Stack/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> stack = new CustomStack<string>();
+
+            string input = "";
+            string[] delimiters = { ",", " " };
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] inputArgs = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                string command = inputArgs[0];
+                if (command == "Push")
+                {
+                    for (int i = 1; i < inputArgs.Length; i++)
+                    {
+                        stack.Push(inputArgs[i]);
+                    }
+                }
+                else if (command == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (InvalidOperationException e)
+                    {
+
+                        Console.WriteLine("No elements");
+                    }
+                }
+            }
+
+            
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
