1a46c0a937f7429ebf90e72ac34bfe9d69c9029b
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-10 12:35:47 +0200

    Created 02. Collection

---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/02.Collection/ListyIterator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>:IEnumerable<T>
+    {
+        private  int _index = 0;
+        private List<T> list;
+        public ListyIterator(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public bool Move()
+        {   
+            if (HasNext()) 
+            {
+                _index++;
+                return true;
+            }
+            return false;
+        }
+        public bool HasNext()
+        {
+            return _index < list.Count-1;
+        }
+
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            Console.WriteLine(list[_index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                //yield return връша резултата от всяка итерация на цикъла
+                yield return list[i];
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Advanced/10.Iterators and Comparators/10.Iterators and Comparators - Exercise/02.Collection/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                           .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            //Директно подаваме на listy стойностите, като скипваме командата
+            ListyIterator<string> listy = new ListyIterator<string>(input.Skip(1).ToList());
+            while ((input[0] = Console.ReadLine()) != "END")
+            {
+                switch (input[0])
+                {
+                    case "HasNext":
+                        //Направо изпечатваме true / false
+                        Console.WriteLine(listy.HasNext());
+                        break;
+                    case "Move":
+                        Console.WriteLine(listy.Move());
+                        break;
+                    case "Print":
+                        //Пробваме да изпринтим, ако не стане, хвърляме exception
+                        try
+                        {
+                            listy.Print();
+                        }
+                        catch (InvalidOperationException ioe)
+                        {
+
+                            Console.WriteLine("Invalid Operation!");
+                        }
+                        break;
+                        //След като сме имплементирали IEnumerable, вече можем да пуснем foreach на PrintAll
+                        //благодарение на yeld return
+                    case "PrintAll":
+                        foreach (var item in listy)
+                        {
+                            Console.Write($"{item} ");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                }
+            }
+        }
+    }
+}
