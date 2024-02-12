2578fa09e4ec12cbd0360fa6d71c1d22ea519996
TeodorIvanov <131561902+TeodorIvanov2304@users.noreply.github.com>
2024-02-12 16:55:14 +0200

    Update 07. Knight Game.cs

--- Advanced/2.Multidimensional Arrays/2.Multidimensional Arrays - Exercise/07. Knight Game.cs
+++ Advanced/2.Multidimensional Arrays/2.Multidimensional Arrays - Exercise/07. Knight Game.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+using System.Data;
 
 namespace _07._Knight_Game
 {
@@ -6,144 +6,174 @@ namespace _07._Knight_Game
     {
         static void Main(string[] args)
         {
-            int size = int.Parse(Console.ReadLine());
 
-            char[,] board = new char[size, size];
+            //КВАДРАТНА МАТРИЦА
+            int size = int.Parse(Console.ReadLine()); //бр.редове = бр. колони
 
-            for (int i = 0; i < size; i++)
+            //? размерът е 1 или 2 -> < 3
+            if (size < 3)
             {
-                string figures = Console.ReadLine();
+                Console.WriteLine(0);
+                return;
+            }
+
+            int countRemoved = 0; //брой премахнати коне
+
+            char[,] matrix = new char[size, size]; //поле за игра
+
+            //Прочитане на матрица от конзолата
+            for (int row = 0; row <= size - 1; row++) //всички редове от първия до последния
+            {
+                char[] symbols = Console.ReadLine().ToArray();
+                for (int col = 0; col <= size - 1; col++)
+                {
+                    matrix[row, col] = symbols[col];
+                }
+            }
+
+            //обхождаме полето и премахваме коне, докато не останем само с добри коне = атакуват 0 други коне
+            while (true)
+            {
+                int maxAttack = 0; //брой на максималните атаки от кон
+                int rowMaxAttack = 0; //ред на най-атакуващия кон
+                int colMaxAttack = 0; //колона на най-атакуващия кон
+
                 for (int row = 0; row < size; row++)
                 {
-
                     for (int col = 0; col < size; col++)
                     {
-                        board[row, col] = figures[col];
-                    }
-                }
-            }
-
-
-            int currentRow = 0;
-            int currentCol = 0;
-            int knightsMoved = 0;
-
-            for (int row = 0; row < size; row++)
-            {
-
-                for (int col = 0; col < size; col++)
-                {
-                    //bool isBoard = row - 1 >= 0
-                    //                && row - 2 >= 0
-                    //                && row + 1 <= size - 1
-                    //                && row + 2 <= size - 1
-                    //                && col - 1 >= 0
-                    //                && col - 2 >= 0
-                    //                && col + 1 <= size - 1
-                    //                && col + 2 <= size - 1;
-
-                    if (board[row, col] == 'K')
-                    {
-                        currentRow = row;
-                        currentCol = col;
-
-                        if (currentCol - 1 >= 0
-                                && currentCol - 1 <= size - 1
-                                && currentCol + 1 >= 0
-                                && currentCol + 1 >= 0
-                                && currentCol + 1 <= size - 1
-                                && currentRow - 1 >= 0
-                                && currentRow - 1 <= size - 1
-                                && currentRow + 1 >= 0
-                                && currentRow + 1 <= size - 1
-                                && currentCol - 2 >= 0
-                                && currentCol - 2 <= size - 1
-                                && currentCol + 2 >= 0
-                                && currentCol + 2 <= size - 1
-                                && currentRow - 2 >= 0
-                                && currentRow - 2 <= size - 1
-                                && currentRow + 2 >= 0
-                                && currentRow + 2 <= size - 1)
+                        char currentElement = matrix[row, col];
+                        if (currentElement == 'K')
                         {
-                            currentRow++;
-                            currentCol += 2;
-
-                            if (board[currentRow,currentCol] == 'K')
+                            //кон
+                            //1. при движения колко коня атакуване (натъпване при преместване)
+                            int countAttackedKnights = CalculateAttackedKnight(row, col, size, matrix);
+                            //2. проверка дали е най-атакуващия 
+                            if (countAttackedKnights > maxAttack) //кой е коня с най-много атаки
                             {
-                                board[row, col] = (char)0;
-                                knightsMoved++;
+                                maxAttack = countAttackedKnights;
+                                rowMaxAttack = row;
+                                colMaxAttack = col;
                             }
+
+
                         }
-
                     }
-
-
-
-
-                    //if (isBoard) 
-                    //{
-                    //    if (board[row, col] == 'K')
-                    //    {
-                    //        if (board[row + 1, col + 2] == 'K')
-                    //        {
-                    //            board[row, col] = '0';
-                    //            knightsMoved++;
-
-                    //        }
-                    //        else if (board[row + 1, col - 2] == 'K')
-                    //        {
-                    //            board[row, col] = '0';
-                    //            knightsMoved++;
-
-                    //        }
-                    //        else if (board[row - 1, col + 2] == 'K')
-                    //        {
-                    //            board[row, col] = '0';
-                    //            knightsMoved++;
-
-                    //        }
-                    //        else if (board[row - 1, col - 2] == 'K')
-                    //        {
-                    //            board[row, col] = '0';
-                    //            knightsMoved++;
-
-                    //        }
-                    //        else if (board[row + 2, col - 1] == 'K')
-                    //        {
-                    //            board[row, col] = '0';
-                    //            knightsMoved++;
-
-                    //        }
-                    //        else if (board[row + 2, col + 1] == 'K')
-                    //        {
-                    //            board[row, col] = '0';
-                    //            knightsMoved++;
-
-                    //        }
-                    //        else if (board[row - 2, col + 1] == 'K')
-                    //        {
-                    //            board[row, col] = '0';
-                    //            knightsMoved++;
-
-                    //        }
-                    //        else if (board[row - 2, col - 1] == 'K')
-                    //        {
-                    //            board[row, col] = '0';
-                    //            knightsMoved++;
-
-                    //        }
-                    //    }
-
-                    //knightsMoved = MoveKnight(board, knightsMoved, row, col);
+                }
+                //намеря най-атакуващия кон
+                if (maxAttack == 0)
+                {
+                    // нямам коне, които да атакуват
+                    break;
+                }
+                else
+                {
+                    //имам кон за премахване
+                    matrix[rowMaxAttack, colMaxAttack] = '0';
+                    countRemoved++;
                 }
             }
-            Console.WriteLine(knightsMoved);
+
+            Console.WriteLine(countRemoved);
+
+
+            //брой на атакуваните коне от текущия кон
+
+            static int CalculateAttackedKnight(int row, int col, int size, char[,] matrix)
+            {
+                int count = 0;
+                //2 нагоре и 1 надясно -> ред - 2, колона + 1
+                if (IsValid(row - 2, col + 1, size))
+                {
+                    //съществуват такива ред и колона -> проверка дали има кон
+                    if (matrix[row - 2, col + 1] == 'K')
+                    {
+                        count++;
+                    }
+                }
+                //2 нагоре и 1 наляво -> ред - 2, кол - 1
+                if (IsValid(row - 2, col - 1, size))
+                {
+                    //съществуват такива ред и колона -> проверка дали има кон
+                    if (matrix[row - 2, col - 1] == 'K')
+                    {
+                        count++;
+                    }
+                }
+                //2 надолу и 1 наляво -> ред + 2, колона - 1
+                if (IsValid(row + 2, col - 1, size))
+                {
+                    //съществуват такива ред и колона -> проверка дали има кон
+                    if (matrix[row + 2, col - 1] == 'K')
+                    {
+                        count++;
+                    }
+                }
+                //2 надолу и 1 надясно -> ред + 2, колона + 1
+                if (IsValid(row + 2, col + 1, size))
+                {
+                    //съществуват такива ред и колона -> проверка дали има кон
+                    if (matrix[row + 2, col + 1] == 'K')
+                    {
+                        count++;
+                    }
+                }
+                //2 надясно и 1 надолу 0 -> ред + 1, колона + 2
+                if (IsValid(row + 1, col + 2, size))
+                {
+                    //съществуват такива ред и колона -> проверка дали има кон
+                    if (matrix[row + 1, col + 2] == 'K')
+                    {
+                        count++;
+                    }
+                }
+
+                //2 надясно и 1 нагоре->ред - 1, колона + 2
+                if (IsValid(row - 1, col + 2, size))
+                {
+                    //съществуват такива ред и колона -> проверка дали има кон
+                    if (matrix[row - 1, col + 2] == 'K')
+                    {
+                        count++;
+                    }
+                }
+
+                // 2 наляво и 1 нагоре -> ред - 1, колона - 2
+                if (IsValid(row - 1, col - 2, size))
+                {
+                    //съществуват такива ред и колона -> проверка дали има кон
+                    if (matrix[row - 1, col - 2] == 'K')
+                    {
+                        count++;
+                    }
+                }
+
+                // 2 наляво и 1 надолу -> ред + 1, колона - 2
+                if (IsValid(row + 1, col - 2, size))
+                {
+                    //съществуват такива ред и колона -> проверка дали има кон
+                    if (matrix[row + 1, col - 2] == 'K')
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+
+            }
+
+            static bool IsValid(int row, int col, int size)
+            {
+                return row >= 0 && row < size && col >= 0 && col < size;
+            }
+
+
         }
 
-        
-        }
 
 
+
+    }
 
 }
 
