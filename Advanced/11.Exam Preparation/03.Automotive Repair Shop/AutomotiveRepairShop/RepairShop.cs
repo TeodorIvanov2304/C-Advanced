b8292602ca47982e067df2292d1f442cdbfc3c98
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-13 20:32:31 +0200

    Create 03. Automotive Repair Shop + description

---
+++ Advanced/11.Exam Preparation/03.Automotive Repair Shop/AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        public RepairShop(int capacity)
+        {
+            Capacity = capacity;
+            this.Vehicles = new List<Vehicle>();
+        }
+
+        public int Capacity { get; set; }
+        public List<Vehicle> Vehicles { get; set; }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if (this.Vehicles.Count < this.Capacity)
+            {
+                this.Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vin)
+        {
+            Vehicle vehicle = Vehicles.FirstOrDefault(v=>v.VIN == vin);
+            if (vehicle != null) 
+            {
+                this.Vehicles.Remove(vehicle);
+                return true;
+            }
+            return false;
+        }
+            
+            
+            //=> Vehicles.Remove(Vehicles.FirstOrDefault(x => x.VIN == vin));
+
+        public int GetCount()
+        {
+            return Vehicles.Count;
+        }
+
+        //Трябваше метода да е от тип Vehicle, и да връща Vehicle, a не стринг!
+        public Vehicle GetLowestMileage() => this.Vehicles.OrderBy(v => v.Mileage).FirstOrDefault();
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Vehicles in the preparatory:");
+            foreach (var vehicle in this.Vehicles)
+            {
+                sb.AppendLine(vehicle.ToString().TrimEnd().TrimStart());
+            }
+
+            return sb.ToString().TrimEnd().TrimStart();
+        }
+    }
+    
+}
