6012ed78c40823abb7ed2afae8479a5fd95aacc4
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-14 20:00:20 +0200

    Created 03. Vending System + problem description

---
+++ Advanced/11.Exam Preparation/03.Vending System/VendingSystem/VendingMachine.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+        public VendingMachine(int buttonCapacity)
+        {
+            ButtonCapacity = buttonCapacity;
+            Drinks = new List<Drink>();
+        }
+
+        public int ButtonCapacity { get; set; }
+        public List<Drink> Drinks { get; set; }
+        public int GetCount => Drinks.Count;
+
+        public void AddDrink(Drink drink)
+        {
+            if (Drinks.Count < ButtonCapacity)
+            {
+                Drinks.Add(drink);
+            }
+        }
+
+        public bool RemoveDrink(string name)
+        {
+            Drink drink = Drinks.FirstOrDefault(n => n.Name == name);
+            if (drink != null)
+            {
+                Drinks.Remove(drink);
+                return true;
+            }
+            return false;
+        }
+
+        public Drink GetLongest()
+        {
+            return Drinks.OrderByDescending(v=>v.Volume).First();
+        }
+
+        public Drink GetCheapest() 
+        {
+            return Drinks.OrderBy(p=>p.Price).First();
+        }
+
+        public string BuyDrink(string name)
+        {
+            return Drinks.FirstOrDefault(n => n.Name == name).ToString();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Drinks available:");
+            foreach (var drink in Drinks)
+            {
+                sb.AppendLine(drink.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
