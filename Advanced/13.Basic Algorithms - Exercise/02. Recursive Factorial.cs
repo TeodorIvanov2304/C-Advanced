ea276e242b2e7b74a229a54c35e8646b98f6b4d6
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-20 19:24:46 +0200

    Create 02. Recursive Factorial.cs

---
+++ Advanced/13.Basic Algorithms - Exercise/02. Recursive Factorial.cs
@@ -0,0 +1,24 @@
+﻿namespace _02._Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Read from the console
+            Console.WriteLine(RecursiveFactorial(int.Parse(Console.ReadLine())));
+        }
+
+        //Method for finding factorial
+        static int RecursiveFactorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            //Use recursion
+            int result = n * RecursiveFactorial(n - 1);
+            return result;
+        }
+    }
+}
