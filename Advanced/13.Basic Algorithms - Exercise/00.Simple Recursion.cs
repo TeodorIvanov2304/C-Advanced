4b038e2a580bc355de8f6478517c9bae746fd9f0
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-20 16:17:35 +0200

    Create 00.Simple Recursion.cs

---
+++ Advanced/13.Basic Algorithms - Exercise/00.Simple Recursion.cs
@@ -0,0 +1,24 @@
+﻿namespace _00.Simple_Recursion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursiveForCycle(10);
+        }
+
+        //Function, that calls itself until is equal to zero
+        static void RecursiveForCycle(int n)
+        {
+            if (n <= 0)
+            {
+                return;
+            }
+            Console.WriteLine($"Before recursion {n}");
+            RecursiveForCycle(n - 1);
+
+            //The recursion allways comes back, in reverse order 
+            Console.WriteLine($"After recursion {n}");
+        }
+    }
+}
