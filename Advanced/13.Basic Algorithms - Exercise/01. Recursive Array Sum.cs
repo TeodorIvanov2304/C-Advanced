a63412fa9a4247fbe7f37ac5f588f840612c6e7e
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-20 16:34:19 +0200

    Create 01. Recursive Array Sum + problems description

---
+++ Advanced/13.Basic Algorithms - Exercise/01. Recursive Array Sum.cs
@@ -0,0 +1,25 @@
+﻿namespace _01._Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+            int index = 0;
+            Console.WriteLine(Sum(array, index));
+        }
+
+        static int Sum(int[] array, int index)
+        {
+            if (index == array.Length-1)
+            {
+                return array[index];
+            }
+
+            return array[index] + Sum(array, index+1);
+        }
+    }
+}
