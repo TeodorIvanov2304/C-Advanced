1b0b33206c461373fcf41b7201bb33f628a67d97
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-20 16:47:16 +0200

    Create 01. Recursive Array Sum Skip.cs

---
+++ Advanced/13.Basic Algorithms - Exercise/01. Recursive Array Sum Skip.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Recursive_Array_Sum_Skip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            Console.WriteLine(ArraySum(array));
+        }
+
+        static int ArraySum(int[] array)
+        {
+            if (array.Length == 1)
+            {
+                return array[0];
+            }
+
+            int[] restOfTheArray = array.Skip(1).ToArray();
+            int restOfArraySum = ArraySum(restOfTheArray);
+
+            return array[0] + restOfArraySum;
+        }
+    }
+}
