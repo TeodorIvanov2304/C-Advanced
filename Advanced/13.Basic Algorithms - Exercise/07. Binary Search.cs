cb77d941997a6580675a1a86d63a8204df4fe7f0
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-21 18:21:22 +0200

    Create 07. Binary Search.cs

---
+++ Advanced/13.Basic Algorithms - Exercise/07. Binary Search.cs
@@ -0,0 +1,55 @@
+﻿namespace _07._Binary_Search
+{
+    
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read input
+            int[] arr = Console.ReadLine()
+                               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+            int key = int.Parse(Console.ReadLine());
+
+            //Print
+            int result = IndexOf(arr, key);
+            Console.WriteLine(result);
+        }
+
+        //Returns the index of the searched element
+        public static int IndexOf(int[] arr, int key)
+        {
+            int lo = 0;
+            int hi = arr.Length - 1;
+
+            while (lo <= hi)
+            {
+                //Find the middle element
+                int middle = (lo + hi) / 2;
+
+                //If key < middle element
+                if (key < arr[middle])
+                {
+                    hi = middle - 1;
+                }
+                //If key > middle element
+                else if (key > arr[middle])
+                {
+                    lo = middle + 1;
+                }
+                //If middle element = key
+                else
+                {
+                    return middle;
+                }
+
+            }
+            //Return - 1, if key not found
+            return -1;
+        }
+
+    }
+    
+}
