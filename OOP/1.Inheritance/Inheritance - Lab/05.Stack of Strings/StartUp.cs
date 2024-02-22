0aad4aeda4d5bb56da3acb4b968773ff6116f464
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-22 16:00:26 +0200

    Created 05.Stack of Strings

---
+++ OOP/1.Inheritance/Inheritance - Lab/05.Stack of Strings/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new string[] { "Yajte", "Pasti" });
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Count);
+        }
+    }
+}
