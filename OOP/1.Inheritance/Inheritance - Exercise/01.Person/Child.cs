62d5bdd04338f028ad412aafe180264e11ffaf93
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-23 15:53:35 +0200

    Created new folder Inheritance Exercise and 01.Person

---
+++ OOP/1.Inheritance/Inheritance - Exercise/01.Person/Child.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+
+        }
+
+        public override int Age 
+        {   
+            //We are getting the age from the base class Person
+            get => base.Age;
+
+            //Cannot set the value over 15
+            set
+            {
+                if (value>15)
+                {
+                    throw new ArgumentException("A child cannot be more than 15");
+                }
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ OOP/1.Inheritance/Inheritance - Exercise/01.Person/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Person
+{
+    public class Person
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        //New syntax
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        //The property is set to virtual, so we can override the Age in Child class
+        public virtual int Age
+        {
+            get => _age;
+            set 
+            { 
+                if (value < 0)
+                {   
+                    //If age < 0, age = 0
+                    //_age = 0;
+                    throw new ArgumentException("Age cannot be negative");
+                }
+                _age = value; 
+            }
+        }
+
+        
+        
+
+        public override string ToString()
+        {
+            
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+
+    }
+}
