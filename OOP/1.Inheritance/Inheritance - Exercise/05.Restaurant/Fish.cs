05fb55940a91013576b50da6c337c1860bf0e05c
TeodorIvanov <131561902+TeodorIvanov2304@users.noreply.github.com>
2024-02-24 21:13:23 +0200

    Update Fish.cs

--- OOP/1.Inheritance/Inheritance - Exercise/05.Restaurant/Fish.cs
+++ OOP/1.Inheritance/Inheritance - Exercise/05.Restaurant/Fish.cs
@@ -1,7 +1,8 @@
 ﻿namespace Restaurant
 {
     public class Fish : MainDish
-    {
+    {    
+        //Също премахваме от конструктора grams, и заместваме в base конструктора grams с _grams
         private const double _grams = 22;
         public Fish(string name, decimal price) : base(name, price, _grams)
         {
