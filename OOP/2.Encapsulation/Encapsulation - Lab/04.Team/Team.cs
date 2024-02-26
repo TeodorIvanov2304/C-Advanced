2f14eeb0bbf6ed41e6c8348e2f5d05af8f84cb8f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-26 18:51:35 +0200

    Create 04.Team

---
+++ OOP/2.Encapsulation/Encapsulation - Lab/04.Team/Team.cs
@@ -0,0 +1,35 @@
+﻿namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+
+
+        //Правим пропъртито IReadOnlyCollection без сетър, за да е енкапсулирано и да не може да се променя. Също така в гетъра слагаме firstTeam и му даваме .AsReadOnly();
+        public IReadOnlyCollection<Person> FirstTeam { get => firstTeam.AsReadOnly();}
+        public IReadOnlyCollection<Person> ReserveTeam { get => reserveTeam.AsReadOnly();}
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                firstTeam.Add(player);
+
+            }
+            else
+            {
+                reserveTeam.Add(player);
+            }
+        }
+    }
+}
