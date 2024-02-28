e9d9ef293360229fc49578ce4ef2654d066e007b
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-02-28 14:27:09 +0200

    Created 03.Pizza Calorie

---
+++ OOP/2.Encapsulation/Encapsulation - Exercise/03.Pizza Calories/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace PizzaCalories.Models
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+
+            try
+            {
+                string[] pizzaInfo = Console.ReadLine().Split();
+                string[] doughInfo = Console.ReadLine().Split();
+
+                Dough dough = new Dough(doughInfo[1], doughInfo[2], double.Parse(doughInfo[3]));
+                Pizza pizza = new Pizza(pizzaInfo[1], dough);
+
+                while ((command = Console.ReadLine()) != "END")
+                {
+                    string[] ingredientInfo = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string typeIngredient = ingredientInfo[0].ToLower();
+
+                    Topping topping = new Topping(ingredientInfo[1], double.Parse(ingredientInfo[2]));
+                    pizza.AddTopping(topping);
+                }
+
+                Console.WriteLine(pizza.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
