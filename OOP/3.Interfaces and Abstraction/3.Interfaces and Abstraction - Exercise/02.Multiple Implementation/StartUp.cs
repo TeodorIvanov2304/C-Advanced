69db26569ec5465818881f9584f127a1964eb820
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-01 22:48:25 +0200

    Created 02.Multiple Implementation

---
+++ OOP/3.Interfaces and Abstraction/3.Interfaces and Abstraction - Exercise/02.Multiple Implementation/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
+
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+
+            Citizen citizen = new Citizen("Grom", 11, "OF22123S", "91.92.22999");
+
+        }
+    }
+}
