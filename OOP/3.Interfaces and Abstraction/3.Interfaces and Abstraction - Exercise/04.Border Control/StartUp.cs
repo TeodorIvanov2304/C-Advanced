80a7f18f615c2400d7f95ff49a95593b7fe94320
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-02 16:12:51 +0200

    Created 04.Border Control

---
+++ OOP/3.Interfaces and Abstraction/3.Interfaces and Abstraction - Exercise/04.Border Control/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using BorderControl.Models;
+
+namespace BorderControl
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Models.BorderControl borderControl = new();
+
+            string input = "";
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] inputTokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                //If Length is 2, we create a robot
+                if (inputTokens.Length == 2)
+                {
+                    Robot robot = new(inputTokens[1], inputTokens[0]);
+                    borderControl.AddForBorderCheck(robot);
+                }
+                //If Length is 3, we create a citizen
+                else
+                {
+                    Citizen citizen = new(inputTokens[2], inputTokens[0], int.Parse(inputTokens[1]));
+                    borderControl.AddForBorderCheck(citizen);
+                }
+            }
+
+            string fakeIdEndSequance = Console.ReadLine();
+            //Using Where and Endswith to find if fakeId digits endings are present in borderControl list
+            var detained = borderControl.EntitiesToBeChecked.Where(e => e.Id.EndsWith(fakeIdEndSequance));
+
+            foreach (var baseEntity in detained)
+            {
+                Console.WriteLine(baseEntity.Id);
+            }
+        }
+    }
+}
