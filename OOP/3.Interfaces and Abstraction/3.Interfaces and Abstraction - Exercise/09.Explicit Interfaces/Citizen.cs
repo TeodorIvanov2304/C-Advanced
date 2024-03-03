bb9fd5e54f99e8e0266366be21b2becf265b3ebb
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-03 17:30:49 +0200

    Create 09.Explicit Interfaces

---
+++ OOP/3.Interfaces and Abstraction/3.Interfaces and Abstraction - Exercise/09.Explicit Interfaces/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using ExplicitInterfaces.Interfaces;
+
+namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string country, int age)
+        {
+            Name = name;
+            Country = country;
+            Age = age;
+        }
+
+        public string Name { get;  }
+
+        public string Country { get;  }
+
+        public int Age { get; }
+
+        //Explicit implementation:Изрично определяме двете имплементации на GetName() в двата варианта IResiden, IPerson
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {Name}";
+        }
+        string IPerson.GetName()
+        {
+            return $"{Name}";
+        }
+    }
+}
