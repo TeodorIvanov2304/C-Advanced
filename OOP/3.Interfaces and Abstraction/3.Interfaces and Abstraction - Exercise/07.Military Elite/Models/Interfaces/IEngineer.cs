64abf45b3a6b85c6157711511bd73c923dfadfbf
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-04 22:24:07 +0200

    Created 07.Military Elite

---
+++ OOP/3.Interfaces and Abstraction/3.Interfaces and Abstraction - Exercise/07.Military Elite/Models/Interfaces/IEngineer.cs
@@ -0,0 +1,5 @@
+﻿namespace MilitaryElite.Models.Interfaces;
+public interface IEngineer : ISpecialisedSoldier
+{
+    IReadOnlyCollection<IRepair> Repairs { get; }
+}
---
+++ OOP/3.Interfaces and Abstraction/3.Interfaces and Abstraction - Exercise/07.Military Elite/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using MilitaryElite.Core;
+using MilitaryElite.Core.Interfaces;
+
+namespace MilitaryElite
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
