463487dced19b5f56c968b14344c58b4871e9398
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-06 17:10:19 +0200

    Create 02.Animals

---
+++ OOP/4.Polymorphism/Polymorphism - Lab/02.Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favoriteFood;
+
+        protected Animal(string name, string favoriteFood)
+        {
+            Name = name;
+            FavoriteFood = favoriteFood;
+        }
+
+        public string Name { get; protected set; }
+        public string FavoriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavoriteFood}";
+        }
+    }
+}
