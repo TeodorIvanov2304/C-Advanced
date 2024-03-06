2cae8910b0e2cc3c8df972617553034f366589b3
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-06 18:42:55 +0200

    Create 03.Shapes

---
+++ OOP/4.Polymorphism/Polymorphism - Lab/03.Shapes/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape newRectnangleShape = new Rectangle(2,2);
+            
+            Console.WriteLine(newRectnangleShape.CalculateArea());
+            Console.WriteLine(newRectnangleShape.CalculatePerimeter());
+            
+            Shape newCircleShape = new Circle(2.3D);
+            Console.WriteLine(newCircleShape.CalculateArea());
+            Console.WriteLine(newCircleShape.CalculatePerimeter());
+        }
+    }
+}
