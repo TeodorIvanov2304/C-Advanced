dc92ffeca3820e97093b9809480bd544cd55d366
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-10 12:17:00 +0200

    Created 5.Exceptions and Error Handling and 2 problems

---
+++ OOP/5.Exceptions and Error Handling/5.Exceptions and Error Handling - Lab/01. Square Root.cs
@@ -0,0 +1,42 @@
+﻿namespace _01._Square_Root
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(SquareRoot(number));
+                
+            }
+            catch (FormatException fe)
+            {
+
+                Console.WriteLine(fe.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        public static double SquareRoot(int number)
+        {
+            CheckForNegativeNumber(number);
+
+            return Math.Sqrt(number);
+        }
+
+        public static void CheckForNegativeNumber(int number)
+        {
+            if (number < 0)
+            {
+                throw new FormatException("Invalid number.");
+            }
+        }
+    }
+
+    
+}
---
+++ OOP/5.Exceptions and Error Handling/5.Exceptions and Error Handling - Lab/02. Enter Numbers.cs
@@ -0,0 +1,53 @@
+﻿namespace _02._Enter_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+            List<int> numbers = new List<int>(10);
+            
+            while (numbers.Count < 10)
+            {
+                
+                try
+                {
+                    int number = ReadNumber(start, end);
+                    numbers.Add(number);
+                    start = number;
+                }
+                catch (FormatException fa)
+                {
+
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch (ArgumentOutOfRangeException ae)
+                {
+                    Console.WriteLine($"Your number is not in range {start} - 100!");
+                }
+            }
+            Console.WriteLine(string.Join(", ",numbers));
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+            string input = Console.ReadLine();
+            int number;
+            bool isGood = int.Parse(input) > start && int.Parse(input) < end;
+            bool IsANumber = int.TryParse(input, out number);
+            if (IsANumber && isGood)
+            {
+                return number;
+            }
+            else if(!IsANumber)
+            {
+                throw new FormatException();
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
