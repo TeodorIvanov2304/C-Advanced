3c43f04bbaffe615f5184520bb67fb526daa8f95
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-11 18:10:07 +0200

    Created 06.CSharp-OOP-SOLID-Lab

---
+++ OOP/6.SOLID/06.OOP-SOLID/P01.Stream_Progress/File.cs
@@ -0,0 +1,12 @@
+﻿namespace P01.Stream_Progress
+{
+    public class File : StreamableFile
+    {
+        private string name;
+
+        public File(string name,int length, int bytesSent) : base(length, bytesSent)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ OOP/6.SOLID/06.OOP-SOLID/P01.Stream_Progress/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            File file = new("KURNIK", 1, 2);
+        }
+    }
+}
