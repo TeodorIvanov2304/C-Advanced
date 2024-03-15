09054faaf4bc48e1630f9d27272cc49634ba3359
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-15 17:33:59 +0200

    Created Reflection and Attributes folder and 06.Code Tracker + problem description

---
+++ OOP/7.Reflection and Attributes/Reflection and Attributes - Lab/06.Code Tracker/AuthorAttribute.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+namespace AuthorProblem
+{
+    //Указваме на атрибута, за кои части от класа ще искаме информация, в случая метод и клас, и му даваме труе на AllowMultiple, за да документира и двата атрибута
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
+
---
+++ OOP/7.Reflection and Attributes/Reflection and Attributes - Lab/06.Code Tracker/Tracker.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Console.WriteLine(Assembly.GetAssembly(typeof(string)).GetTypes().Length);
+            foreach (var type in Assembly.GetAssembly(typeof(Int32)).GetTypes())
+            {
+                Console.WriteLine(type.FullName);
+            }
+
+            Type[] allTypes = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => t == typeof(StartUp)).ToArray();
+
+            foreach (var type in allTypes)
+            {
+                PrintAllMethodAuthors(type);
+                continue;
+            }
+        }
+
+        private void PrintAllMethodAuthors(Type type)
+        {
+            MethodInfo[] methods = type.GetMethods((BindingFlags)60);
+            foreach (var method in methods)
+            {
+                if (!method.GetCustomAttributes().Any(a => a.GetType() == typeof(AuthorAttribute)))
+                {
+                    continue;
+                }
+
+                Attribute[] attributes = method.GetCustomAttributes().ToArray();
+
+                foreach (var attr in attributes)
+                {
+                    if (attr is AuthorAttribute)
+                    {
+                        AuthorAttribute author = (AuthorAttribute)attr;
+                        Console.WriteLine($"{method.Name} is written by {author.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
