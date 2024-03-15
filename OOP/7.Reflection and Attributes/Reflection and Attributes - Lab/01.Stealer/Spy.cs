4e986951d2a0b4a3f15020308e90ded1e273d4d2
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-15 23:46:04 +0200

    Create 01.Stealer

---
+++ OOP/7.Reflection and Attributes/Reflection and Attributes - Lab/01.Stealer/Spy.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {   
+            //Взимаме типа на класа, който ще разследваме
+            Type typeClass = Type.GetType(investigatedClass);
+
+            //Правим масив от тип FieldInfo и използваме метода GetFields, и енумерацията BindingFlags за да вземем fields от класа Hacker. В случая Instance,Public,NonPublic members of the class
+            FieldInfo[] classFields = typeClass.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            //Алтернативно изписване
+            //FieldInfo[] classFields = typeClass.GetFields((BindingFlags)60);
+            StringBuilder sb = new StringBuilder();
+
+            //Създаваме обект от подадения клас, чрез класа Activator и метода CreateInstance
+            Object classInstance = Activator.CreateInstance(typeClass, new object[] { });
+
+            
+            sb.AppendLine($"Class under investigation: {investigatedClass}");
+
+            //Всички филдове в колекцията classFields, които съдържа името на част от подадените полета
+            foreach (FieldInfo field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                //Добавяме към StringBuilder
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ OOP/7.Reflection and Attributes/Reflection and Attributes - Lab/01.Stealer/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            //Създавме променлива, която ще пази резултата от StringBuilder, като параметри подаваме метода името на класа, и името на филдовете, които търсим
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
