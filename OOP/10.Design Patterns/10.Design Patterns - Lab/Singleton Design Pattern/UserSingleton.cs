086ab08962e6c99474c296f142131f014fe838b3
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-03-25 12:11:22 +0200

    Update UserSingleton.cs

--- OOP/10.Design Patterns/10.Design Patterns - Lab/Singleton Design Pattern/UserSingleton.cs
+++ OOP/10.Design Patterns/10.Design Patterns - Lab/Singleton Design Pattern/UserSingleton.cs
@@ -4,6 +4,8 @@
     {   
         //Правим реалнта единствена инстанция!
         private static UserSingleton instance;
+        //Правим обект, който ще използваме за заключване
+        private static object instanceLock = new object();
         //Правим private конструктор
         private UserSingleton()
         {
@@ -17,8 +19,16 @@
             {   
                 //В този if, ще влезнем само веднъж, при създаването на инстанцията
                 if (instance == null)
-                {
-                    instance = new UserSingleton();
+                {   
+                    //Заключваме инстанцията, така, че да има само една създадена инстанция само 1 път
+                    lock (instanceLock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new UserSingleton();
+                        }
+                    }
+                    
                 }
 
                 return instance;
