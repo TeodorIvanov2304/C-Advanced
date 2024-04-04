c9d566c10a3c495a40585a08b349de11bad5ac82
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-04-04 12:42:50 +0300

    Created 03.Vending Retail + Tests

---
+++ OOP/11.Exam Preparation/03.Vending Retail/VendingRetail/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VendingRetail
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CoffeeMat cm = new(1000, 2);
+            Console.WriteLine(cm.FillWaterTank());
+        }
+    }
+}
