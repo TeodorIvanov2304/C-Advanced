6579236fdfc043b091779c7ad783c6fc71426951
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-04-05 15:00:13 +0300

    Created 03.Vehicle Garage + Tests

---
+++ OOP/11.Exam Preparation/03.Vehicle Garage/VehicleGarage/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VehicleGarage
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle vehicle = new("Ford", "Mustang", "CB1111BB");
+            Garage garage = new(10);
+            garage.AddVehicle(vehicle);
+            garage.DriveVehicle("CB1111BB",99,false);
+
+        }
+    }
+}
